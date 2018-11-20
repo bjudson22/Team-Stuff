dacbc4f7c508875e5ffdee856e0009d7bd48cb84
b.judson <b.judson@hotmail.com>
2018-11-19 19:35:22 -0600

    changed name

--- TeamBruh/TeamBruh/Person.cs
+++ TeamBruh/TeamBruh/Person.cs
@@ -8,7 +8,9 @@ namespace TeamBruh
 {
     class Person
     {
-        public string Firstame { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Dankname { get; set; }
         public string address { get; set; }
     }
 }
